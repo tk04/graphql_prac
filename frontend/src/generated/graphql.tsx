eca117aaee6f8d1cd612ef7395c4288c734c01fb
taloufi <taloufi@iu.edu>
2022-02-17 11:53:06 -0500

    added login page & urql mutation

--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -96,6 +96,13 @@ export type UsernamePasswordInput = {
   username: Scalars['String'];
 };
 
+export type LoginMutationVariables = Exact<{
+  options: UsernamePasswordInput;
+}>;
+
+
+export type LoginMutation = { __typename?: 'Mutation', login: { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null, user?: { __typename?: 'User', id: number, username: string } | null } };
+
 export type RegisterMutationVariables = Exact<{
   username: Scalars['String'];
   password: Scalars['String'];
@@ -105,6 +112,24 @@ export type RegisterMutationVariables = Exact<{
 export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null, user?: { __typename?: 'User', id: number, username: string } | null } };
 
 
+export const LoginDocument = gql`
+    mutation Login($options: UsernamePasswordInput!) {
+  login(options: $options) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+    `;
+
+export function useLoginMutation() {
+  return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
+};
 export const RegisterDocument = gql`
     mutation Register($username: String!, $password: String!) {
   register(options: {username: $username, password: $password}) {
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import Wrapper from "../components/Wrapper";
+import Inputfield from "../components/InputField";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+interface loginProps {}
+
+const Login: React.FC<loginProps> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+  return (
+    <Wrapper varient="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          console.log(values);
+          const response = await login({ options: values });
+          if (response.data.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data.login.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Inputfield
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <Inputfield
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+              color="white"
+            >
+              {" "}
+              Login{" "}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
