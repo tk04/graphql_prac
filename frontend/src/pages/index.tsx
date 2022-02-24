8a2141bc53d2a4c4123ce5593b91399919fe3942
taloufi <taloufi@iu.edu>
2022-02-23 22:15:32 -0500

    added single post page and adjust post query to include creator

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -40,7 +40,11 @@ const Index = () => {
             <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
               <Upvotesection post={p} />
               <Box>
-                <Heading fontSize="xl">{p.title}</Heading>
+                <NextLink href="/post/[id]" as={`/post/${p.id}`}>
+                  <Link>
+                    <Heading fontSize="xl">{p.title}</Heading>
+                  </Link>
+                </NextLink>
                 <Text>posted by {p.creator.username}</Text>
                 <Text mt={4}>{p.textSnippet.slice(0, 50)}</Text>
               </Box>
---
+++ frontend/src/pages/post/[id].tsx
@@ -0,0 +1,30 @@
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import Layout from "../../components/Layout";
+import { usePostQuery } from "../../generated/graphql";
+import { createUrqlClient } from "../../utils/createUrqlClient";
+
+interface PostProps {}
+
+const Post: React.FC<PostProps> = ({}) => {
+  const router = useRouter();
+  const intId =
+    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+  if (fetching) {
+    return (
+      <Layout>
+        <div>loading...</div>
+      </Layout>
+    );
+  }
+  return <Layout>{data?.post?.text}</Layout>;
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
